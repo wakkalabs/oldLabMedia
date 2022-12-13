71f7e045ff4104f287261ef555e895990b1cd9fa
acarter-ims <austin.carter@intelerad.com>
2022-12-12 21:15:31 -0600

    Cleanup and pushing master

--- MediaLibraryV2/config/helper/add-nat-routing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-IPT="/sbin/iptables"
-IPT6="/sbin/ip6tables"
-
-
-iptables -A INPUT -p udp -m udp --dport 51194 -j ACCEPT
-iptables -A FORWARD -i wg0 -j ACCEPT
-iptables -A FORWARD -o wg0 -j ACCEPT
